68ccee997e3a221e320cb4e3cb0cd704891b71af
jhy <jinghongyi.jhy@alibaba-inc.com>
2023-06-05 15:49:16 +0800

    py3

--- bert.py
+++ bert.py
@@ -1,7 +1,7 @@
 import numpy as np
 from transformers import BertTokenizer
-text1 = "我们的爱可以重来，[SEP]我们的爱值得等待。"
-text2 = "我们的爱可以重来，我们的爱值得等待。"
+text1 = "我们爱可以重来，[SEP]我们的爱值得等待。"
+text2 = "我们爱可以重来，我们的爱值得等待。"
 text3 = "[CLS]"
 tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 print(tokenizer(text1)['input_ids'])
