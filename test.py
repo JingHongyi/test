a495377de1e174d8ccd587d813c140ca233c8e5b
Jing Hongyi <37770306+JingHongyi@users.noreply.github.com>
2023-06-12 16:28:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import json
 file = open('test.json','r')
 cons = json.load(file)
-print("name:{}\nage:{}school:{}\ncity:{}\n".format(cons['name'],cons['age'],cons['school'],cons['city']))
+print("name:{}\nage:{}school:{}\n".format(cons['name'],cons['age'],cons['school']))
