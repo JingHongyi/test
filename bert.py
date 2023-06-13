74c4e6156b17b05f4f7196a59727d0f3308da4ec
jhy <jinghongyi.jhy@alibaba-inc.com>
2023-06-13 15:54:57 +0800

    1

--- bert.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from transformers import BertTokenizer
-text1 = "我们的爱可以重来，[SEP]我们的爱值得等待。"
-text2 = "我们的爱可以重来，我们的爱值得等待。"
-text3 = "[CLS]"
-tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
-print(tokenizer(text1)['input_ids'])
-print(tokenizer(text2)['input_ids'])
--- test.py
+++
@@ -1,4 +0,0 @@
-import json
-file = open('test.json','r')
-cons = json.load(file)
-print(cons['2'])
